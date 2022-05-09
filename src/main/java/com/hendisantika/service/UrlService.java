3a476751f6c297da3d6c4c53c49a23e755e9d298
Hendi Santika <hendisantika@gmail.com>
2022-05-10 05:04:28 +0700

    #10 - Add generateShortLink in UrlService

--- src/main/java/com/hendisantika/service/UrlService.java
+++ src/main/java/com/hendisantika/service/UrlService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.service;
 
+import com.hendisantika.dto.UrlDTO;
+import com.hendisantika.model.Url;
 import com.hendisantika.repository.UrlRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UrlService {
     private final UrlRepository urlRepository;
+
+    public Url generateShortLink(UrlDTO urlDto) {
+        if (StringUtils.isNotEmpty(urlDto.getUrl())) {
+            String encodedUrl = encodeUrl(urlDto.getUrl());
+            Url urlToPersist = new Url();
+            urlToPersist.setCreationDate(LocalDateTime.now());
+            urlToPersist.setOriginalUrl(urlDto.getUrl());
+            urlToPersist.setShortLink(encodedUrl);
+            urlToPersist.setExpirationDate(getExpirationDate(urlDto.getExpirationDate(),
+                    urlToPersist.getCreationDate()));
+            Url urlToRet = persistShortLink(urlToPersist);
+
+            return urlToRet;
+        }
+        return null;
+    }
 }
