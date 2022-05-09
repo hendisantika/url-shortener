42eada3628c0f371640a7dd57180b4684889bf30
Hendi Santika <hendisantika@gmail.com>
2022-05-10 05:09:54 +0700

    #17 - Add generateShortLink API in UrlShorteningController

--- src/main/java/com/hendisantika/controller/UrlShorteningController.java
+++ src/main/java/com/hendisantika/controller/UrlShorteningController.java
@@ -1,8 +1,17 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.dto.UrlDTO;
+import com.hendisantika.dto.UrlErrorResponseDto;
+import com.hendisantika.dto.UrlResponseDto;
+import com.hendisantika.model.Url;
 import com.hendisantika.service.UrlService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +26,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("urls")
 public class UrlShorteningController {
     private final UrlService urlService;
 
+    @PostMapping("/generate")
+    public ResponseEntity<?> generateShortLink(@RequestBody UrlDTO urlDto) {
+        Url urlToRet = urlService.generateShortLink(urlDto);
+
+        if (urlToRet != null) {
+            UrlResponseDto urlResponseDto = new UrlResponseDto();
+            urlResponseDto.setOriginalUrl(urlToRet.getOriginalUrl());
+            urlResponseDto.setExpirationDate(urlToRet.getExpirationDate());
+            urlResponseDto.setShortLink(urlToRet.getShortLink());
+            return new ResponseEntity<>(urlResponseDto, HttpStatus.OK);
+        }
+
+        UrlErrorResponseDto urlErrorResponseDto = new UrlErrorResponseDto();
+        urlErrorResponseDto.setStatus("404");
+        urlErrorResponseDto.setError("There was an error processing your request. please try again.");
+        return new ResponseEntity<>(urlErrorResponseDto, HttpStatus.OK);
+
+    }
 }
